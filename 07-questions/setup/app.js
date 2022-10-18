74d689470d3497ff8bac86badf3fa04a9bcc0f09
Don-Alvin <alvindon41@gmail.com>
2022-10-18 15:33:33 +0300

    question challenge done

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,22 @@
 //using selectors inside the element
+const questions = document.querySelectorAll(".question");
+questions.forEach((question) => {
+	const btn = question.querySelector(".question-btn");
+	btn.addEventListener("click", () => {
+		questions.forEach((item) => {
+			if (item !== question) {
+				item.classList.remove("show-text");
+			}
+		});
+		question.classList.toggle("show-text");
+	});
+});
+
 // traversing the dom
+// const questionBtns = document.querySelectorAll(".question-btn");
+// questionBtns.forEach((btn) => {
+// 	btn.addEventListener("click", (e) => {
+// 		const question = e.currentTarget.parentElement.parentElement;
+// 		question.classList.toggle("show-text");
+// 	});
+// });
